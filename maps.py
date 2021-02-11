073e0ace7d7024d196ace501600d8c6c313f5f17
anniebrass03 <60437200+anniebrass03@users.noreply.github.com>
2021-02-11 10:37:30 -0600

    Maps and modules
    Here are the maps and modules for my Crime Solver RPG and a short run-through of the first part of the game.

---
+++ maps.py
@@ -0,0 +1,7 @@
+# Both lists for map for the RPG
+
+# Items in FBI Map
+data =[['Patty Rose', '  Clue'], ['START', '\t\t  Emily Joy'], ['Blake Morris', 'LAB']]
+
+# Items in Lab map
+data_1 = [['Pencil', 'FBI'], ['START', ' Blue fabric'], ['Clue', '  Hair']]
