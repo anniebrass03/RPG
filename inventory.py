1e9ba808b7401f9b406a5c10aeb7677adc28ac92
anniebrass03 <60437200+anniebrass03@users.noreply.github.com>
2021-02-11 11:02:39 -0600

    Modules and maps
    A short run-through of the RPG, Crime Solver. Including two maps and multiple modules for inventory, characters, maps, map choices, and main.

---
+++ inventory.py
@@ -0,0 +1,6 @@
+# Inventory of evidence
+evidence = ['Pencil', 'Hair', 'Blue Fabric']
+
+print ("\nHere is the inventory of evidence:")
+print('\n'.join(evidence))
+print ("\nYou can examine the evidence by going to the Lab map.\n")
