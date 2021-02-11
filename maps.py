fb42e77e5155b0cb38c7a7af2641d5d45ffa8a5a
anniebrass03 <60437200+anniebrass03@users.noreply.github.com>
2021-02-11 10:42:06 -0600

    Delete maps.py

--- maps.py
+++
@@ -1,7 +0,0 @@
-# Both lists for map for the RPG
-
-# Items in FBI Map
-data =[['Patty Rose', '  Clue'], ['START', '\t\t  Emily Joy'], ['Blake Morris', 'LAB']]
-
-# Items in Lab map
-data_1 = [['Pencil', 'FBI'], ['START', ' Blue fabric'], ['Clue', '  Hair']]
