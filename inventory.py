38ccd7126d5d63fc2ac760962c147290ea560071
anniebrass03 <60437200+anniebrass03@users.noreply.github.com>
2021-02-11 10:42:40 -0600

    Delete inventory.py

--- inventory.py
+++
@@ -1,6 +0,0 @@
-# Inventory of evidence
-evidence = ['Pencil', 'Hair', 'Blue Fabric']
-
-print ("\nHere is the inventory of evidence:")
-print('\n'.join(evidence))
-print ("You can examine the evidence by going to the Lab map.\n")
